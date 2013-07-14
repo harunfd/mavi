47e148adfb01c222e6e559305ba57b8321532e59
kaptan <kaptan.gokcan@gmail.com>
2013-07-14 14:48:21 +0300

    first

---
+++ pages/hakkinda detay.php
@@ -0,0 +1,36 @@
+<a class="back-trigger" href="pages/bilgi.html">&lt;</a>
+<div class="page-content">
+      <h2>Mavi Masa</h2>
+      <div class="sub-content">
+        
+        <div class="blog-article">
+          <div class="article-header">
+            <p class="title"></p>       
+            <p class="text">Ankara’nın çözüm merkezi ve hizmet sunumunda yeni bir dönem yaratan Mavi Masa çağdaş belediyecilik anlayışımızın gereği olarak Ankaralı vatandaşlarımıza daha iyi hizmet verebilmek ve aramızdaki iletişimi kuvvetlendirmek için 2003 yılında kurulmuş olup çalışmalarımız hakkındaki görüş, düşünce, şikâyet ve tavsiyeler doğrultusunda 2009 yılında yeni bir sisteme geçmiş ve bağlantı noktalarını artırmıştır.</p>
+        </div>
+          <div class="article-header">
+            <p class="title">Mavi Masa Nedir? </p>       
+            <p class="text">Vatandaşlarımızın 7 gün/24 saat ulaşarak sorun, görüş ve taleplerini iletebildiği Hizmet Birimi’dir.</p>
+        </div>
+         <div class="article-header">
+            <p class="title"> İlkelerimiz</p>       
+           <ul class="bullet-2">
+ 
+    <li>Vatandaş memnuniyeti</li>
+    <li>Kaliteli hizmet</li>
+    <li>Karşılıklı iletişim</li>
+    <li>Hızlı çözüm üretme,</li>
+    <li>Etkin denetleme</li>
+    <li>Gizlilik</li>
+ 
+		</ul>
+        </div>
+        
+          <div class="article-footer">
+            <a onclick="history.back(); return false;" href="#" class="button button2 left">Geri</a>
+         
+            <div class="clear"></div>
+          </div>
+        </div>        
+      </div>
+</div>
